4c5697cc17a08a44a1bb41fa202fdf1fa0503b02
Max <maxsoderquist@gmail.com>
2018-01-30 17:01:58 +0100

    boo

--- init.el
+++ init.el
@@ -4,12 +4,9 @@
 
 ;; Define package repositories
 (require 'package)
+
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/"))
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
